d796ec83acbe080532dc790a101adfe9daeb44b3
xsaven <gms.sw.xsaven@gmail.com>
2020-05-28 03:53:03 +0300

    Changed: assets/js.dev/ljs.js, assets/js.dev/plugins/alert.js, assets/js.dev/plugins/fancy.js, assets/js.dev/plugins/jq.js, assets/js.dev/plugins/mask.js, assets/js.dev/plugins/nav.js, assets/js.dev, assets/js.dev/plugins/vue.js, assets/js/ljs.js, assets/js/plugins/alert.js, assets/js/plugins/fancy.js, assets/js/plugins/jq.js, assets/js/plugins/mask.js, assets/js/plugins/nav.js, assets/js/plugins/vue.js, javascript/Helper.tsx, javascript/ljs/classes/LJS.tsx

--- assets/js.dev/plugins/alert.js
+++ assets/js.dev/plugins/alert.js
@@ -297,6 +297,39 @@ var Helper = /** @class */ (function (_super) {
             return urlParams;
         }
     };
+    /**
+     * Number formatter
+     * @param num
+     * @param decimals
+     * @param dec_point
+     * @param thousands_sep
+     */
+    Helper.number_format = function (num, decimals, dec_point, thousands_sep) {
+        if (decimals === void 0) { decimals = 0; }
+        if (dec_point === void 0) { dec_point = '.'; }
+        if (thousands_sep === void 0) { thousands_sep = ','; }
+        var i, j, kw, kd, km;
+        if (isNaN(decimals = Math.abs(decimals))) {
+            decimals = 2;
+        }
+        if (dec_point === undefined) {
+            dec_point = ",";
+        }
+        if (thousands_sep === undefined) {
+            thousands_sep = ".";
+        }
+        i = parseInt(num = (+num || 0).toFixed(decimals)) + "";
+        if ((j = i.length) > 3) {
+            j = j % 3;
+        }
+        else {
+            j = 0;
+        }
+        km = (j ? i.substr(0, j) + thousands_sep : "");
+        kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+        kd = (decimals ? dec_point + Math.abs(num - parseInt(i)).toFixed(decimals).replace(/-/, '0').slice(2) : "");
+        return km + kw + kd;
+    };
     return Helper;
 }(Conditions_1.Conditions));
 exports.Helper = Helper;
--- javascript/Helper.tsx
+++ javascript/Helper.tsx
@@ -189,4 +189,24 @@ export class Helper extends Conditions {
             return urlParams;
         }
     }
+
+    /**
+     * Number formatter
+     * @param num
+     * @param decimals
+     * @param dec_point
+     * @param thousands_sep
+     */
+    static number_format(num: any, decimals: number = 0, dec_point: string = '.', thousands_sep: string = ',' ) {
+        let i, j, kw, kd, km;
+        if( isNaN(decimals = Math.abs(decimals)) ){ decimals = 2; }
+        if( dec_point === undefined ){ dec_point = ","; }
+        if( thousands_sep === undefined ){ thousands_sep = "."; }
+        i = parseInt(num = (+num || 0).toFixed(decimals)) + "";
+        if( (j = i.length) > 3 ){ j = j % 3; } else{ j = 0; }
+        km = (j ? i.substr(0, j) + thousands_sep : "");
+        kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+        kd = (decimals ? dec_point + Math.abs(num - parseInt(i)).toFixed(decimals).replace(/-/, '0').slice(2) : "");
+        return km + kw + kd;
+    }
 }
